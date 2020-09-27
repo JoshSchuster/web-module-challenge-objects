e137d5548645ef36b6e5682775ec71ac884a7de3
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-09-27 18:52:46 -0500

    completes task 7

--- index.js
+++ index.js
@@ -101,10 +101,12 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
+function getLastReview(arrayOfReviews) {
+    return arrayOfReviews[arrayOfReviews.length-1].name + " gave the restaurant a " + arrayOfReviews[arrayOfReviews.length-1].rating + " star review and their feedback was: " + arrayOfReviews[arrayOfReviews.length-1].feedback;
   } 
 
+  reviews.pop(); //remove my review so we can return Reyna's review
+  console.log(getLastReview(reviews)); //test
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
